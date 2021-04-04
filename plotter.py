155c174bcd3342976752d08eb97a6a886ebf8a4b
ChatrikMangat <chatriksinghmangat@gmail.com>
2021-04-04 14:51:20 +0530

    fancy visualisation added

--- plotter.py
+++ plotter.py
@@ -12,12 +12,19 @@ for i in fout:
 	if(i[0]==0):
 		continue
 	if(np.isnan(i[3])):
-		ly = np.linspace(bounds[0][1]-1,bounds[1][1]+1,3)
+		ly = np.linspace(fin[int(i[1]-1)][1],fin[int(i[0]-1)][1],3)
 		lx = [fin[int(i[1]-1)][0] for k in range(len(ly))]
 	else:
-		lx = np.linspace(bounds[0][0]-1,bounds[1][0]+1,3)
+		lx = np.linspace(fin[int(i[1]-1)][0],fin[int(i[0]-1)][0],3)
 		ly = i[3]*lx+i[4]
-	plt.plot(lx,ly, color="red")
+	plt.plot(lx,ly, color="lime")
+
+for i in range(len(fout)-1):
+	if(i==0):
+		continue
+	plt.plot([fin[int(fout[i][1])-1][0],fin[int(fout[i+1][0])-1][0]],[fin[int(fout[i][1])-1][1],fin[int(fout[i+1][0])-1][1]],color="red",linestyle="--")
+
+plt.title('Total cost = '+str(fout[1][2]), loc="center")
 
 plt.savefig("currplot")
 plt.show()
